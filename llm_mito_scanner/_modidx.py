de3cae72d06b38c3c2f8a188165e5ae42afcc788
Jake Bergren <jdbergren@gmail.com>
2023-05-08 20:53:39 -0400

    Wrote code to stage reference genome in sqlite

--- llm_mito_scanner/_modidx.py
+++ llm_mito_scanner/_modidx.py
@@ -5,4 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://DataAndDeadlifts.github.io',
                 'git_url': 'https://github.com/DataAndDeadlifts/llm-mito-scanner',
                 'lib_path': 'llm_mito_scanner'},
-  'syms': {'llm_mito_scanner.core': {'llm_mito_scanner.core.foo': ('core.html#foo', 'llm_mito_scanner/core.py')}}}
+  'syms': { 'llm_mito_scanner.features.genome': { 'llm_mito_scanner.features.genome.format_sequence_for_insert': ( 'make-genome-training-data.html#format_sequence_for_insert',
+                                                                                                                   'llm_mito_scanner/features/genome.py'),
+                                                  'llm_mito_scanner.features.genome.insert_genome_into_sqlite': ( 'make-genome-training-data.html#insert_genome_into_sqlite',
+                                                                                                                  'llm_mito_scanner/features/genome.py'),
+                                                  'llm_mito_scanner.features.genome.insert_sequences': ( 'make-genome-training-data.html#insert_sequences',
+                                                                                                         'llm_mito_scanner/features/genome.py')}}}
